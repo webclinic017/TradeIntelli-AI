7058a44b250f78896a964fa7ea23e85f793654a1
omerSuliman <omerahmed41@gmail.com>
2024-03-25 01:35:25 +0100

    -  separate controller file.

---
+++ alpca-consumer/app/historical_data_controller.py
@@ -0,0 +1,28 @@
+from fastapi import Query, APIRouter
+
+from app.data_formatter import DataFormatter
+from domain import enums
+from domain.historical_data_retriever import HistoricalDataRetriever
+from domain.indicators import Indicators
+import traceback
+
+
+router = APIRouter()
+
+
+@router.get("/historical-data")
+async def get_historical_data(stock: str = Query('BTC'), time_frame: str = Query("1D")):
+    try:
+        historical_data_retriever = HistoricalDataRetriever()
+        symbol = enums.symbol_map.get(stock.lower())
+
+        historical_data = historical_data_retriever.get_historical_data(stock, time_frame)
+
+        Indicators.add_ema(historical_data)
+        Indicators.calculate_resistance_and_support(historical_data)
+
+        return DataFormatter.formate_data(historical_data, symbol)
+
+    except Exception as e:
+        traceback.print_exc()
+        return []
--- alpca-consumer/main.py
+++ alpca-consumer/main.py
@@ -1,31 +1,9 @@
-from alpaca.data.timeframe import TimeFrame
-from fastapi import Query
-
-from app.data_formatter import DataFormatter
-from domain import enums
-from domain.indicators import Indicators
+# from app import historical_data_controller
 from infastructure.fast_api_app import FastAPIApp
-from domain.historical_data_retriever import HistoricalDataRetriever
-import traceback
 
+from app.historical_data_controller import router as historical_data_router
 app = FastAPIApp.init_app()
-historical_data_retriever = HistoricalDataRetriever()
 
-
-@app.get("/historical-data")
-async def get_historical_data(stock: str = Query('BTC'), time_frame: str = Query("1D")):
-    try:
-        symbol = enums.symbol_map.get(stock.lower())
-
-        historical_data = historical_data_retriever.get_historical_data(stock, time_frame)
-
-        Indicators.add_ema(historical_data)
-        Indicators.calculate_resistance_and_support(historical_data)
-
-        return DataFormatter.formate_data(historical_data, symbol)
-
-    except Exception as e:
-        traceback.print_exc()
-        return []
+app.include_router(historical_data_router)
 
 
