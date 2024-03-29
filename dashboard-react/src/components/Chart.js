44fb7b8a6bbcb938f57ce89c59305f7ef49c368f
omerSuliman <omerahmed41@gmail.com>
2024-03-29 23:13:54 +0100

    - added drawing.

--- dashboard-react/src/components/Chart.js
+++ dashboard-react/src/components/Chart.js
@@ -1,5 +1,5 @@
-// src/App.js
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import CanvasDraw from "react-canvas-draw";
 import CandlestickChart from './CandlestickChart';
 import StockSelector from './StockSelector';
 import TimeFrameSelector from './TimeFrameSelector';
@@ -8,43 +8,71 @@ import './Chart.css';
 
 function Chart({ id }) {
     const [selectedStock, setSelectedStock] = useState('BTC');
-    const [selectedTimeFrame, setSelectedTimeFrame] = useState('5M'); // Default to 1D
-    const [selectedStartDate, setSelectedStartDate] = useState('60'); // Default to 1D
+    const [selectedTimeFrame, setSelectedTimeFrame] = useState('5M');
+    const [selectedStartDate, setSelectedStartDate] = useState('60');
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [isDrawingEnabled, setIsDrawingEnabled] = useState(false);
+    const drawingCanvasRef = useRef(null);
 
+
+    const clearDrawing = () => {
+        drawingCanvasRef.current.clear();
+    };
     useEffect(() => {
         const fetchData = async () => {
-            setIsLoading(true); // Start loading
+            setIsLoading(true);
             try {
-                const response = await fetch('http://127.0.0.1:8000/historical-data/?stock='
-                 + selectedStock + '&time_frame=' + selectedTimeFrame+ '&start_date=' + selectedStartDate);
+                const response = await fetch(`http://127.0.0.1:8000/historical-data/?stock=${selectedStock}&time_frame=${selectedTimeFrame}&start_date=${selectedStartDate}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-               const jsonData = await response.json();
+                const jsonData = await response.json();
                 setData(jsonData);
             } catch (error) {
                 console.error('Failed to fetch data:', error);
             }
-            setIsLoading(false); // End loading
+            setIsLoading(false);
         };
 
         fetchData();
     }, [selectedStock, selectedTimeFrame, selectedStartDate]);
 
+    const toggleDrawing = () => setIsDrawingEnabled(!isDrawingEnabled);
+
     return (
-            <div className="App">
-            <div className="toolbar" id={id+"x"}>
+        <div className="App">
+            <div className="toolbar" id={`${id}x`}>
                 <StockSelector onSelect={setSelectedStock} />
                 <StartDateSelector selectedStartDate={selectedStartDate} onSelectStartDate={setSelectedStartDate} />
                 <TimeFrameSelector selectedTimeFrame={selectedTimeFrame} onSelectTimeFrame={setSelectedTimeFrame} />
+                <button onClick={toggleDrawing}>{isDrawingEnabled ? 'Stop Drawing' : 'Start Drawing'}</button>
+                <button onClick={clearDrawing}>Clear Drawing</button>
             </div>
-                {isLoading ? <p>Loading...</p> : <CandlestickChart data={data} id={id}/>}
-            </div>
-        );
-    }
-
+            {isLoading ? <p>Loading...</p> : (
+                <div className="chartWithDrawing">
+                    <CandlestickChart data={data} id={id}/>
+                    <CanvasDraw
+                        ref={drawingCanvasRef}
+                        disabled={!isDrawingEnabled}
+                        style={{
+                            position: 'absolute',
+                            top: 0,
+                            left: 0,
+                            zIndex: 10,
+                            pointerEvents: isDrawingEnabled ? 'all' : 'none',
+                            // Temporary background color for visibility when drawing is enabled
+                            backgroundColor: isDrawingEnabled ? 'rgba(255,255,255,0.3)' : 'transparent',
+                        }}
+                        canvasWidth={1600}
+                        canvasHeight={800}
+                        hideGrid={true}
+                        brushRadius={4}
+                    />
+                </div>
+            )}
+        </div>
+    );
+}
 
 export default Chart;
-
