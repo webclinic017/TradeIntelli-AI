79d6d354f386973aa9c1e13449ecb1ea28dfae0f
omerSuliman <omerahmed41@gmail.com>
2024-04-09 15:07:46 +0400

    - send email if Bearish.

--- alpca-consumer/alerts.py
+++ alpca-consumer/alerts.py
@@ -1,6 +1,8 @@
 from app.domain.historical_data_retriever import HistoricalDataRetriever
 from app.domain.indicators import Indicators
 
+from app.app.historical_data_controller import send_email
+
 
 class Alerts:
 
@@ -11,8 +13,13 @@ class Alerts:
     @staticmethod
     def run_alerts():
         print("running alerts:")
-        market_direction_5m = Alerts.get_market_direction("btc", "5M")
-        print(f"market_direction: {market_direction_5m}")
+        market_direction_5m = Alerts.get_market_direction("ndx100", "5M")
+        market_direction_30m = Alerts.get_market_direction("ndx100", "30M")
+        print(f"market_direction 5m: {market_direction_5m}")
+        print(f"market_direction 30m: {market_direction_30m}")
+        if market_direction_5m == "Bearish" and market_direction_5m == "Bearish":
+            subject = f"Trading alert: ndx100 is {market_direction_5m}"
+            send_email("ndx100", subject)
 
     @staticmethod
     def get_market_direction(stock, time_frame):
@@ -24,3 +31,5 @@ class Alerts:
         Indicators.calculate_resistance_and_support(historical_data)
         Indicators.decide_market_direction(historical_data)
         return historical_data["market_direction"][-1]
+
+
