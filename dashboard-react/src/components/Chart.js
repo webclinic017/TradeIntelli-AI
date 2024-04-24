75e82ba28eb04437a5b35bda1742a699c4980715
omerSuliman <omerahmed41@gmail.com>
2024-04-24 09:37:59 +0200

    - replaced 127.0.0.1 with localhost.

--- dashboard-react/src/components/Chart.js
+++ dashboard-react/src/components/Chart.js
@@ -25,7 +25,7 @@ function Chart({ id }) {
         const fetchData = async () => {
             setIsLoading(true);
             try {
-                const response = await fetch(`http://127.0.0.1:8000/historical-data/?stock=${selectedStock}&time_frame=${selectedTimeFrame}&start_date=${selectedStartDate}`);
+                const response = await fetch(`http://localhost:8000/historical-data/?stock=${selectedStock}&time_frame=${selectedTimeFrame}&start_date=${selectedStartDate}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
