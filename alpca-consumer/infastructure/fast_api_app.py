1989084606b0d09ea1ed34e3a9d621a58bac8b23
omerSuliman <omerahmed41@gmail.com>
2024-04-24 10:44:10 +0200

    - updated CORSMiddleware.

--- alpca-consumer/infastructure/fast_api_app.py
+++ alpca-consumer/infastructure/fast_api_app.py
@@ -22,7 +22,7 @@ class FastAPIApp:
 
             FastAPIApp.app.add_middleware(
                 CORSMiddleware,
-                allow_origins=origins,
+                allow_origins=["*"],
                 allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"],
